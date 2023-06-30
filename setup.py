44ae7fcde4b80c95d05fe6016a6128094cbe74ee
Devin Gaffney <itsme@devingaffney.com>
2023-06-30 08:37:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import setup, Extension
+import subprocess
+
+# Run the necessary commands
+subprocess.run(["cmake", "-DCMAKE_BUILD_TYPE=Release", "-DBUILD_TOOLS=ON", "./chromaprint"])
+subprocess.run(["make"], cwd="./chromaprint")
+subprocess.run(["make", "install"], cwd="./chromaprint")
+
+# Create symbolic links
+subprocess.run(["rm", "/usr/lib/x86_64-linux-gnu/libchromaprint.so.1.5.0"])
+subprocess.run(["rm", "/usr/lib/x86_64-linux-gnu/libchromaprint.so.1"])
+subprocess.run(["ln", "-s", "/usr/local/lib/libchromaprint.so.1.5.0", "/usr/lib/x86_64-linux-gnu/libchromaprint.so.1.5.0"])
+subprocess.run(["ln", "-s", "/usr/local/lib/libchromaprint.so.1", "/usr/lib/x86_64-linux-gnu/libchromaprint.so.1"])
+
 
 setup(
     name='chromaprint',
