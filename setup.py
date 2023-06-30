4c6d57a7bf669f599aee8c33311ebabe8fd5740c
Devin Gaffney <itsme@devingaffney.com>
2023-06-30 08:39:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, Extension
+import os
 import subprocess
 
+# Get the absolute path of the current directory
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Set the path to the chromaprint source directory
+chromaprint_dir = os.path.join(current_dir, "chromaprint")
+
 # Run the necessary commands
-subprocess.run(["cmake", "-DCMAKE_BUILD_TYPE=Release", "-DBUILD_TOOLS=ON", "./chromaprint"])
-subprocess.run(["make"], cwd="./chromaprint")
-subprocess.run(["make", "install"], cwd="./chromaprint")
+subprocess.run(["cmake", "-DCMAKE_BUILD_TYPE=Release", "-DBUILD_TOOLS=ON", "."], cwd=chromaprint_dir)
+subprocess.run(["make"], cwd=chromaprint_dir)
+subprocess.run(["make", "install"], cwd=chromaprint_dir)
 
 # Create symbolic links
 subprocess.run(["rm", "/usr/lib/x86_64-linux-gnu/libchromaprint.so.1.5.0"])
@@ -12,7 +19,7 @@ subprocess.run(["rm", "/usr/lib/x86_64-linux-gnu/libchromaprint.so.1"])
 subprocess.run(["ln", "-s", "/usr/local/lib/libchromaprint.so.1.5.0", "/usr/lib/x86_64-linux-gnu/libchromaprint.so.1.5.0"])
 subprocess.run(["ln", "-s", "/usr/local/lib/libchromaprint.so.1", "/usr/lib/x86_64-linux-gnu/libchromaprint.so.1"])
 
-
+# Define the extension module
 setup(
     name='chromaprint',
     version='1.0.0',
